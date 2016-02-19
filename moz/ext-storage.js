b66af120d1fae9f990c8f6b143f3a94e124eac93
Michiel de Jong <mbdejong@mozilla.com>
2016-02-19 14:20:29 +0000

    Add ExtensionStorageSync (just a copy of ~Local)

--- moz/ext-storage.js
+++ moz/ext-storage.js
@@ -3,6 +3,9 @@ var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 XPCOMUtils.defineLazyModuleGetter(this, "ExtensionStorageLocal",
                                   "resource://gre/modules/ExtensionStorageLocal.jsm");
 
+XPCOMUtils.defineLazyModuleGetter(this, "ExtensionStorageSync",
+                                  "resource://gre/modules/ExtensionStorageSync.jsm");
+
 Cu.import("resource://gre/modules/ExtensionUtils.jsm");
 var {
   EventManager,
@@ -35,10 +38,34 @@ extensions.registerPrivilegedAPI("storage", (extension, context) => {
         },
       },
 
+      sync: {
+        get: function(keys, callback) {
+          ExtensionStorageSync.get(extension.id, keys).then(result => {
+            runSafe(context, callback, result);
+          });
+        },
+        set: function(items, callback) {
+          ExtensionStorageSync.set(extension.id, items).then(() => {
+            if (callback) {
+              runSafe(context, callback);
+            }
+          });
+        },
+        remove: function(items, callback) {
+          ExtensionStorageSync.remove(extension.id, items).then(() => {
+            if (callback) {
+              runSafe(context, callback);
+            }
+          });
+        },
+      },
+
       onChanged: new EventManager(context, "storage.onChanged", fire => {
-        ExtensionStorage.setEventManager(extension.id, fire);
+        ExtensionStorageLocal.setEventManager(extension.id, fire);
+        ExtensionStorageSync.setEventManager(extension.id, fire);
         return () => {
-          ExtensionStorage.removeEventManager(extension.id, fire);
+          ExtensionStorageLocal.removeEventManager(extension.id);
+          ExtensionStorageSync.removeEventManager(extension.id);
         };
       }).api(),
     },
